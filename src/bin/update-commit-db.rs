c23a8b2a89f45a70f1b93dc921c35fe193fb7b14
Jakub Matraszek <jakub.matraszek@gmail.com>
2017-01-26 18:14:02 +0100

    8: update-commit-db for multiple projects

--- src/bin/update-commit-db.rs
+++ src/bin/update-commit-db.rs
@@ -18,6 +18,7 @@ extern crate serde_json;
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use dotenv::dotenv;
+use reqwest::Url;
 
 use std::env;
 
@@ -50,26 +51,25 @@ pub fn establish_connection() -> PgConnection {
         .expect(&format!("Error connecting to {}", database_url))
 }
 
-fn main() {
+fn update_commit_db(github_name: &str, connection: &PgConnection) {
     use contributors::schema::releases::dsl::*;
     use contributors::models::Release;
     use contributors::schema::commits::dsl::*;
     use contributors::models::Commit;
 
-    let connection = establish_connection();
-
-    let mut resp = reqwest::get("https://api.github.com/repos/rust-lang/rust/commits").unwrap();
+    let api_link = Url::parse(format!("https://api.github.com/repos/{}/commits", github_name).as_str()).unwrap();
+    let mut resp = reqwest::get(api_link).unwrap();
 
     let response: GitHubResponse = resp.json().unwrap();
 
     // find the master release so we can assign commits to it
-    let master_release = releases.filter(version.eq("master")).first::<Release>(&connection).expect("could not find release");
+    let master_release = releases.filter(version.eq("master")).first::<Release>(connection).expect("could not find release");
 
     for object in response.0 {
         println!("Found commit with sha {}", object.sha);
 
         // do we have this commit? If so, ignore it.
-        match commits.filter(sha.eq(&object.sha)).first::<Commit>(&connection) {
+        match commits.filter(sha.eq(&object.sha)).first::<Commit>(connection) {
             Ok(commit) => {
                 println!("Commit {} already in db, skipping", commit.sha);
                 continue;
@@ -78,8 +78,19 @@ fn main() {
                 println!("Creating commit {} for release {}", object.sha, master_release.version);
 
                 // this commit will be part of master
-                contributors::create_commit(&connection, &object.sha, &object.commit.author.name, &object.commit.author.email, &master_release);
+                contributors::create_commit(connection, &object.sha, &object.commit.author.name, &object.commit.author.email, &master_release);
             },
         };
+    }
+}
+
+fn main() {
+    use contributors::schema::projects::dsl::*;
+    use contributors::models::Project;
+    let connection = establish_connection();
+    let projects_to_update: Vec<Project> = projects.load(&connection).expect("No projects found");
+    for project in projects_to_update {
+        println!("Updating {}", project.name);
+        update_commit_db(project.github_name.as_str(), &connection)
     }
 }

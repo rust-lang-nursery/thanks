2c983d268410650e3101273d3a171751a3e9d000
Steve Klabnik <steve@steveklabnik.com>
2017-01-27 15:45:47 -0500

    Properly calculate position in all-time rank
    This code is a bit tricky; but it lets us have people with duplicate
    commit counts have a duplicate rank. So for example:
    
      14 Name1 5
      15 Name2 6
      15 Name3 6
      17 Name4 7
    
    Since Name2 and Name3 have the same number of commits, they're tied for
    rank 15. But that means Name4 is rank 17, not rank 16.
    
    Fixes #32

--- src/lib.rs
+++ src/lib.rs
@@ -217,8 +217,29 @@ pub fn scores() -> Vec<Value> {
         .load(&connection)
         .unwrap();
 
+    // these variables are used to calculate the ranking
+    let mut rank = 0; // incremented every time
+    let mut last_rank = 0; // the current rank
+    let mut last_score = 0; // the previous entry's score
+
     scores.into_iter().map(|(author, score)| {
+        // we always increment the ranking
+        rank += 1;
+
+        // if we've hit a different score...
+        if last_score != score {
+
+            // then we need to save these values for the future iteration
+            last_rank = rank;
+            last_score = score;
+        }
+
         let mut json_score: BTreeMap<String, Value> = BTreeMap::new();
+
+        // we use last_rank here so that we get duplicate ranks for people
+        // with the same number of commits
+        json_score.insert("rank".to_string(), Value::I64(last_rank));
+
         json_score.insert("author".to_string(), Value::String(author));
         json_score.insert("commits".to_string(), Value::I64(score));
 

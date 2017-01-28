3b5d64503a5eaf40ba887ce82665949c5557195c
Jakub Matraszek <jakub.matraszek@gmail.com>
2017-01-28 10:18:28 +0100

    8: Implement deleting single project

--- src/bin/the-big-red-button.rs
+++ src/bin/the-big-red-button.rs
@@ -40,11 +40,43 @@ fn main() {
         true => delete_whole_db(&log, &connection),
         false => {
             match matches.value_of("project_name") {
-                Some(project_name) => info!(&log, "Project name: {}", project_name),
+                Some(project_name) => delete_projects_db(&log, &connection, project_name),
                 None => println!("No project specified"),
             };
         }
     }
+}
+
+fn delete_projects_db(log: &slog::Logger, connection: &PgConnection, project_name: &str) {
+    use contributors::schema::releases::dsl::*;
+    use contributors::models::Release;
+    use contributors::schema::projects::dsl::{projects, name};
+    use contributors::models::Project;
+    use contributors::schema::commits::dsl::{commits, release_id};
+    use diesel::expression::dsl::any;
+
+    let project = projects.filter(name.eq(project_name)).first::<Project>(connection).expect("Unknown project!");
+    let releases_to_delete = Release::belonging_to(&project).load::<Release>(connection).unwrap();
+    let release_names: Vec<i32> = releases_to_delete.iter().map(|ref release| release.id).collect();
+    let release_ids: Vec<i32> = releases_to_delete.iter().map(|ref release| release.id).collect();
+    info!(log, "Deleting project {} with release names: {:?}", project_name, release_names);
+
+    info!(log, "Deleting commits");
+    diesel::delete(commits.filter(release_id.eq(any(&release_ids))))
+        .execute(connection)
+        .expect("Error deleting releases");
+
+    info!(log, "Deleting releases");
+    diesel::delete(releases.filter(id.eq(any(&release_ids))))
+        .execute(connection)
+        .expect("Error deleting releases");
+
+    info!(log, "Deleting projects");
+    diesel::delete(projects.filter(name.eq(project_name)))
+        .execute(connection)
+        .expect("Error deleting projects");
+
+    info!(log, "Done.");
 }
 
 fn delete_whole_db(log: &slog::Logger, connection: &PgConnection) {
@@ -68,5 +100,4 @@ fn delete_whole_db(log: &slog::Logger, connection: &PgConnection) {
         .expect("Error deleting projects");
 
     info!(log, "Done.");
-
 }

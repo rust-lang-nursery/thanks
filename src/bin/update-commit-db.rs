5b09fc2d1a02ca01bdd5af794072d53f8802668d
Jakub Matraszek <jakub.matraszek@gmail.com>
2017-01-28 13:36:31 +0100

    8: Fix update-commit-db

--- src/bin/update-commit-db.rs
+++ src/bin/update-commit-db.rs
@@ -15,9 +15,8 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 
-use diesel::prelude::*;
 use diesel::pg::PgConnection;
-use dotenv::dotenv;
+use diesel::prelude::*;
 use reqwest::Url;
 #[macro_use]
 extern crate slog;
@@ -25,7 +24,7 @@ extern crate slog_term;
 
 use slog::DrainExt;
 
-use diesel::prelude::*;
+use contributors::models::Project;
 
 #[derive(Debug,Deserialize)]
 struct GitHubResponse(Vec<Object>);
@@ -47,41 +46,41 @@ struct Author {
     email: String,
 }
 
-pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url))
-}
-
-fn update_commit_db(github_name: &str, connection: &PgConnection) {
+fn update_commit_db(log: &slog::Logger, project: &Project, connection: &PgConnection) {
     use contributors::schema::releases::dsl::*;
     use contributors::models::Release;
     use contributors::schema::commits::dsl::*;
     use contributors::models::Commit;
+    use diesel::expression::dsl::any;
 
-    let api_link = Url::parse(format!("https://api.github.com/repos/{}/commits", github_name).as_str()).unwrap();
+    let api_link = Url::parse(format!("https://api.github.com/repos/{}/commits", project.github_name).as_str()).unwrap();
     let mut resp = reqwest::get(api_link).unwrap();
 
     let response: GitHubResponse = resp.json().unwrap();
 
     // find the master release so we can assign commits to it
-    let master_release = releases.filter(version.eq("master")).first::<Release>(connection).expect("could not find release");
+    let master_release = releases
+        .filter(project_id.eq(project.id))
+        .filter(version.eq("master"))
+        .first::<Release>(connection)
+        .expect("could not find release");
+
+    let release_ids: Vec<i32> = Release::belonging_to(project).load::<Release>(connection).unwrap()
+        .iter().map(|ref release| release.id).collect();
 
     for object in response.0 {
         info!(log, "Found commit with sha {}", object.sha);
 
-        // do we have this commit? If so, ignore it.
-        match commits.filter(sha.eq(&object.sha)).first::<Commit>(connection) {
+        match commits
+            .filter(release_id.eq(any(&release_ids)))
+            .filter(sha.eq(&object.sha))
+            .first::<Commit>(connection) {
             Ok(commit) => {
                 info!(log, "Commit {} already in db, skipping", commit.sha);
                 continue;
             },
             Err(_) => {
                 info!(log, "Creating commit {} for release {}", object.sha, master_release.version);
-
                 // this commit will be part of master
                 contributors::create_commit(connection, &object.sha, &object.commit.author.name, &object.commit.author.email, &master_release);
             },
@@ -91,12 +90,13 @@ fn update_commit_db(github_name: &str, connection: &PgConnection) {
 
 fn main() {
     let log = slog::Logger::root(slog_term::streamer().full().build().fuse(), o!("version" => env!("CARGO_PKG_VERSION")));
+
     use contributors::schema::projects::dsl::*;
-    use contributors::models::Project;
-    let connection = establish_connection();
+
+    let connection = contributors::establish_connection();
     let projects_to_update: Vec<Project> = projects.load(&connection).expect("No projects found");
     for project in projects_to_update {
-        println!("Updating {}", project.name);
-        update_commit_db(project.github_name.as_str(), &connection)
+        info!(log, "Updating {}", project.name);
+        update_commit_db(&log, &project, &connection)
     }
 }

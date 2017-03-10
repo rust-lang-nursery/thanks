2002e0ae4c4a665b2d905b4e4ebac6a4d839aa8d
steveklabnik <steve@steveklabnik.com>
2017-03-10 11:41:00 -0500

    run migrations in a build.rs
    This means we should never run destructive migrations unless it contains only migrations
    
    thanks @sgrif

---
+++ build.rs
@@ -0,0 +1,21 @@
+extern crate diesel;
+extern crate dotenv;
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+
+use dotenv::dotenv;
+
+use std::env;
+
+fn main() {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    let connection = PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url));
+
+    diesel::migrations::run_pending_migrations(&connection)
+        .expect("oh no migrations couldn't be run");
+}

b78fb80a46539b5966e2dc5c333a6be0e20f540f
steveklabnik <steve@steveklabnik.com>
2017-04-05 16:57:32 -0400

    do git stuff

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 extern crate futures;
 extern crate sparkles;
 extern crate thanks;
+extern crate git2;
 
 mod routes;
 
+use std::fs;
+use std::path::PathBuf;
+
 fn main() {
+    let checkout = PathBuf::from("tmp/rust");
+    let url = "https://github.com/rust-lang/rust";
+
+    println!("Initializing");
+
+    let repo = match git2::Repository::open(&checkout) {
+        Ok(r) => r,
+        Err(..) => {
+            println!("Fetching repo");
+            let _ = fs::remove_dir_all(&checkout);
+            fs::create_dir_all(&checkout).unwrap();
+            git2::build::RepoBuilder::new()
+                .bare(true)
+                .clone(&url, &checkout).unwrap()
+        }
+    };
+
+    println!("Starting up.");
+
     let mut server = sparkles::Server::new("templates".to_string());
 
     server.add_route("/", routes::root);
